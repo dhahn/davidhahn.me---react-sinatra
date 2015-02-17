83bb609f706896469c73c23247776b6e08b84b0e
David Hahn <davidmichaelhahn@gmail.com>
2015-02-16 22:39:16 -0600

    Add kinda responsive stuff

--- public/scripts.js
+++ public/scripts.js
@@ -1,33 +1,49 @@
 $(document).ready(function(){ 
 
-	var makerCount = -1,
-	    makerWordArray = JSON.parse(window.config.maker_words),
+  var makerCount = -1,
+      makerWordArray = JSON.parse(window.config.maker_words),
       makerTimeout = setTimeout(makerTimeoutFunction, 1000);
+
   function makerTimeoutFunction() {
-		var new_word = makerWordArray[++makerCount % makerWordArray.length];
-		$("#maker-of-things .keyword").html(new_word);
+    var new_word = makerWordArray[++makerCount % makerWordArray.length];
+    $("#maker-of-things .keyword").html(new_word);
     makerTimeout = setTimeout(makerTimeoutFunction, 1000);
   }
 
   var toolsCount = -1,
-	    toolsWordArray = JSON.parse(window.config.tools_words),
-	    toolsTimeout = setTimeout(toolsTimeoutFunction, 1000);
+      toolsWordArray = JSON.parse(window.config.tools_words),
+      toolsTimeout = setTimeout(toolsTimeoutFunction, 1000);
+
   function toolsTimeoutFunction() {
-		var new_word = toolsWordArray[++toolsCount % toolsWordArray.length];
-		$("#make-things-with .keyword").html(new_word);
+    var new_word = toolsWordArray[++toolsCount % toolsWordArray.length];
+    $("#make-things-with .keyword").html(new_word);
     toolsTimeout = setTimeout(toolsTimeoutFunction, 250);
   }
 
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
 
-	    var target = this.hash;
-	    var $target = $(target);
+    var target = this.hash;
+    var $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
+
+  $('#project-wrapper').bxSlider({
+    minSlides: 1,
+    maxSlides: 4,
+    controls: true,
+    speed: 700,
+    mode: 'horizontal',
+    slideWidth: 260,
+    slideMargin: 0,
+    pager: false,
+    captions: false,
+    infiniteLoop: true
+  });
+
 });
